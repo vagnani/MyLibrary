48c1394469c934beaf085803483fd5435c9b6b68
vagnani <vagnani01@gmail.com>
2016-02-27 14:47:29 +0100

    bugfix grafo added a new file: chessboard

---
+++ MyLibrary/Collections/Chess.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Collections
+{
+    public class MyChessBoard : IEnumerable<List<Coordinate>>
+    {
+        internal Coordinate _start;
+        internal Coordinate _arrive;
+
+        internal Coordinate _limit;
+        internal List<Coordinate> _locked;
+        internal Dictionary<string, Coordinate> _award;
+        internal List<string> _directions;
+
+        public MyChessBoard() { }
+        public MyChessBoard( Coordinate start, Coordinate arrive, Coordinate limit,
+         List<Coordinate> locked, Dictionary<string, Coordinate> award,List<string> directions)
+        {
+            _start = start; _arrive = arrive;_limit = limit;_locked = locked; _award = award;_directions = directions;
+        }
+
+        public IEnumerator<List<Coordinate>> GetEnumerator()
+        {
+            return new MyEnumerator(this);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class MyEnumerator : IEnumerator<List<Coordinate>>
+    {
+        private int index = -1;
+        private List<List<Coordinate>> _listMax;
+        private List<List<Coordinate>> final;
+        private MyChessBoard chess;
+
+        public MyEnumerator() { }
+        public MyEnumerator(MyChessBoard chess):this()
+        {
+            this.chess = chess;
+            _listMax.Add(new List<Coordinate>() { chess._start });
+            SetAll(chess._start, new List<Coordinate>() {chess._start}, 0);
+        }
+
+        public List<Coordinate> Current
+        {
+            get
+            {
+                return final[index];
+            }
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if(index>=final.Count)
+            {
+                return false;
+            }
+
+            index++;
+            return true;
+        }
+
+        private void SetAll(Coordinate _first, List<Coordinate> locked, int index)
+        {
+            var copyListMax = CopyFrom(_listMax[index]);
+            int copyIndex = index;
+
+            foreach (var nodePossible in chess._directions)
+            {                
+                List<Coordinate> copyLocked = CopyFrom(locked);
+                var possibleCoordinate = MoveByDirection(nodePossible, _first);
+
+                if (possibleCoordinate<chess._limit && 
+                    !chess._locked.Contains(possibleCoordinate, possibleCoordinate) && 
+                    !locked.Contains(possibleCoordinate, possibleCoordinate))
+                {
+                    if (copyIndex != index)
+                    {
+                        _listMax.Add(new List<Coordinate>(copyListMax));
+                        copyIndex = _listMax.Count - 1;
+                    }
+
+                    _listMax[copyIndex].Add(possibleCoordinate);
+                    copyLocked.Add(possibleCoordinate);
+                    SetAll(possibleCoordinate, copyLocked, copyIndex);
+                    index++;
+                }
+            }
+        }
+        
+        private Coordinate MoveByDirection(string direction,Coordinate coor)
+        {
+            #region est 
+            if (direction=="nne")
+            {
+                return coor + new int[] { 1, +2 };
+            }
+
+            if (direction == "ene")
+            {
+                return coor + new int[] { 2, +1 };
+            }
+
+            if (direction == "ese")
+            {
+                return coor + new int[] { 2, -1 };
+            }
+
+            if (direction == "sse")
+            {
+                return coor + new int[] { 1, -2 };
+            }
+            #endregion
+
+            #region ovest
+            if (direction == "nno")
+            {
+                return coor + new int[] { -1, +2 };
+            }
+
+            if (direction == "ono")
+            {
+                return coor + new int[] { -2, +1 };
+            }
+
+            if (direction == "oso")
+            {
+                return coor + new int[] { -2, -1 };
+            }
+
+            if (direction == "sso")
+            {
+                return coor + new int[] { -1, -2 };
+            }
+            #endregion
+
+            return new Coordinate(0, 0);
+        }
+
+        private T CopyFrom<T>(T list) where T : class, IEnumerable, ICollection, IList, new()
+        {
+            T result = new T();
+            foreach (var n in list)
+            {
+                result.Add(n);
+            }
+            return result;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~MyEnumerator() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+
+    public struct Coordinate:IEqualityComparer<Coordinate>
+    {
+        int x;
+        int y;
+
+        public Coordinate(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator >(Coordinate obj1,Coordinate obj2)
+        {
+            if(obj1.x>obj2.x && obj1.y>obj2.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator <(Coordinate obj1, Coordinate obj2)
+        {
+            if (obj1.x < obj2.x && obj1.y < obj2.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var obj1 = (Coordinate)obj;
+            return this.x==obj1.x && this.y==obj1.y;
+        }
+
+        #region IEqualityCmparer
+        public bool Equals(Coordinate x, Coordinate y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(Coordinate obj)
+        {
+            return (obj.x + obj.y).GetHashCode();
+        }
+        #endregion
+
+        #region Operator
+        public static bool operator ==(Coordinate obj1, Coordinate obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(Coordinate obj1, Coordinate obj2)
+        {
+            if(obj1.Equals(obj2))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+        public static Coordinate operator +(Coordinate obj1,int[] xy)
+        {
+            return new Coordinate(obj1.x + xy[0], obj1.y + xy[1]);
+        }
+        #endregion
+    }
+}
--- MyLibrary/Collections/Grafo.cs
+++ MyLibrary/Collections/Grafo.cs
@@ -1,179 +1,363 @@
-﻿//using MyLibrary.Collections;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
-//namespace MyLibrary.Collections.Grafo
-//{
-//    public class MyLinkedList : IEnumerable<List<MyLinkedListNode>>
-//    {
-//        internal MyLinkedListNode _first;
-//        internal MyLinkedListNode _goal;
-//        internal int _count = 1;
+namespace MyLibrary.Collections.Grafo
+{
+    public class MyLinkedList : IEnumerable<List<MyLinkedListNode>>
+    {
+        internal MyLinkedListNode _first;
+        internal MyLinkedListNode _goal;
+        internal List<MyLinkedListNode> _allNode;
+        internal List<string> _allNodeString;
 
-//        public MyLinkedList(MyLinkedListNode first, MyLinkedListNode last)
-//        {
-//            _first = first; _goal = last;
-//        }
+        public MyLinkedList(MyLinkedListNode first, MyLinkedListNode last)
+        {
+            _first = first; _goal = last;
+            _allNode = new List<MyLinkedListNode>();
+            _allNodeString = new List<string>();
+            _allNode.Add(first); _allNode.Add(last);
+        }
 
-//        public IEnumerator<List<MyLinkedListNode>> GetEnumerator()
-//        {
-//            throw new NotImplementedException();
-//        }
+        public MyLinkedListNode First
+        {
+            get
+            {
+                return _first;
+            }
+        }
+        public MyLinkedListNode Goal
+        {
+            get
+            {
+                return _goal;
+            }
+        }
 
-//        IEnumerator IEnumerable.GetEnumerator()
-//        {
-//            throw new NotImplementedException();
-//        }
-//    }
+        public IEnumerator<List<MyLinkedListNode>> GetEnumerator()
+        {
+            return (IEnumerator<List<MyLinkedListNode>>)(new MyEnumerator(this));
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
-//    public class MyEnumerator : IEnumerator<List<MyLinkedListNode>>
-//    {
-//        private MyLinkedListNode _first;
-//        private MyLinkedListNode _last;
-//        private List<MyLinkedListNode> _current;
+        public void AddAfter(MyLinkedListNode node, MyLinkedListNode newNode)
+        {
+            var found = _allNode.FindIndex(x => x.Equals(node));
+            _allNode[found]._next.Add(newNode);
+            if (!_allNode.Contains(newNode, newNode))
+                _allNode.Add(newNode);
+        }
 
-//        private List<int> _followingIndex;
-//        private readonly int _countFirst;
+        public void Add(MyLinkedListNode node)
+        {
+            _first._next.Add(node);
+            if (!_allNode.Contains(node, node))
+                _allNode.Add(node);
+        }
 
-//        private bool stop = false;
-//        private bool changeIndexHead = false;
+        public void AddLast(MyLinkedListNode node)
+        {
+            node._next.Add(_goal);
+        }
 
-//        //ricordarsi di stop
-//        #region other
-//        public MyEnumerator(MyLinkedList mylink)
-//        {
-//            this._first = mylink._first;
-//            _last = mylink._goal;
-//            _countFirst = _first.NextCount;
+        public void AddString(string str)
+        {
+            str.Trim();
 
-//        }
+            char excluded1 = '(';
+            char excluded2 = ')';
+            char excluded3 = ',';
 
-//        object IEnumerator.Current
-//        {
-//            get
-//            {
-//                return Current;
-//            }
-//        }
-//        public List<MyLinkedListNode> Current
-//        {
-//            get
-//            {
-//                if (!stop)
-//                {
-//                    return _current;
-//                }
+            List<List<string>> all = new List<List<string>>();
 
-//                throw new IndexOutOfRangeException();
-//            }
-//        }
+            for (int index = 0; index < str.Count(); index++)
+            {
+                List<string> temp = new List<string>();
 
-//        public void Dispose()
-//        {
-//            throw new NotImplementedException();
-//        }
-//        #endregion
+                //if (str[index] == excluded1)                
+                string name = "";
+                for (; index <= str.Count(); index++)
+                {
+                    if (str[index] != excluded1 && str[index] != excluded2 && str[index] != excluded3) //exception
+                    {
+                        name += str[index];
+                    }
 
-//        public bool MoveNext()
-//        {
-//            if (_current == null | _current.Count <= 2)
-//            {
-//                _current.Add(_first);
-//            }
+                    if (str[index] == excluded3)
+                    {
+                        temp.Add(name);
+                        name = "";
+                    }
+
+                    if (str[index] == excluded2)
+                    {
+                        temp.Add(name);
+                        break;
+                    }
+                }
+
+                all.Add(temp);
+            }
+
+            List<string> mustContain = new List<string>() { _first.name };
+            int countDone = 0;
+            //int indexMust = 0;
 
 
-//            List<string> locked = new List<string>();
-//            MyLinkedListNode current = null;
+            //to see again the function of islock, i mean when i must lock the iterazione
+            for (int indexMust = 0; countDone < all.Count; indexMust++)
+            {
+                List<List<string>> rightToAdd = new List<List<string>>();
 
-//            //Implementare aggiunta di tutte le classi     
-//            //locked.Add(_first._next,false)
-//            int _currentGrade =0;
+                foreach (var list in all)
+                {
+                    if (list[0] == mustContain[indexMust])
+                    {
+                        rightToAdd.Add(list);
+                    }
+                }
 
-//            while(true)
-//            {
-//               _currentGrade++;
-//               if(_currentGrade>_followingIndex.Count)
-//               {
-//                    for (int n = 0; n < current.NextCount; n++)
-//                    {
-//                        var currentName = current.Next(n).name;
+                if (rightToAdd.Count > 0)
+                {
+                    foreach (var list in rightToAdd)
+                    {
+                        SetAllNodeString();
 
-//                        if (locked.Contains(currentName)== false)
-//                        {
-//                            _followingIndex.Add(n);
-//                            current = current.Next(n);
-//                            _current.Add(current);
-//                        }
-//                    }
-//                }
-//            }
-            
+                        //if(_allNodeString.Contains(list[0]))
+                        int indexFather = _allNodeString.FindIndex(x => x == list[0]);
 
-//            return false;
-//        }
+                        if (_allNodeString.Contains(list[1]))
+                        {
+                            int indexSon = _allNodeString.FindIndex(x => x == list[1]);
+                            AddAfter(_allNode[indexFather], _allNode[indexSon]);
+                            _allNode[indexFather].value.Add(list[1], Convert.ToInt32(list[2]));
+                            mustContain.Add(list[1]);
+                            countDone++;
+                        }
+                        else
+                        {
+                            var node = new MyLinkedListNode(list[1], new Dictionary<string, int>());
+                            //check if it is added to _allNode
+                            AddAfter(_allNode[indexFather], node);
+                            _allNode[indexFather].value.Add(list[1], Convert.ToInt32(list[2]));
+                            mustContain.Add(list[1]);
+                            countDone++;
+                        }
+                    }
+                }
+            }
+        }
 
-//        public void Reset()
-//        {
-//            throw new NotImplementedException();
-//        }
-//    }
+        private void SetAllNodeString()
+        {
+            _allNodeString = new List<string>();
+            foreach (var str in _allNode)
+            {
+                _allNodeString.Add(str.name);
+            }
 
-//    //rivedre metodo di blocco (isLock) per muoversi
-//    public class MyLinkedListNode
-//    {
-//        internal List<MyLinkedListNode> _next;
-//        internal List<MyLinkedListNode> _prev;
-//        internal bool _isLock = false;
-//        internal readonly string name;
-//        internal Dictionary<string, int> value;
+        }
+    }
 
-//        public MyLinkedListNode(string name, Dictionary<string, int> value)
-//        {
-//            this.name = name;
-//            this.value = value;
-//        }
+    public class MyEnumerator : IEnumerator<List<MyLinkedListNode>>
+    {
+        private MyLinkedListNode _first;
+        private MyLinkedListNode _last;
+        private int _index = -1;
 
-//        public MyLinkedListNode Next(int n)
-//        {
-//            return _next[n];
-//        }
+        private List<List<MyLinkedListNode>> _listMax = new List<List<MyLinkedListNode>>();
+        private List<List<MyLinkedListNode>> _finalList = new List<List<MyLinkedListNode>>();
 
-//        public MyLinkedListNode Previous(int n)
-//        {
-//            return _prev[n];
-//        }
+        public List<MyLinkedListNode> Current
+        {
+            get
+            {
+                if (_index < _finalList.Count)
+                    return _finalList[_index];
+                throw new IndexOutOfRangeException();
+            }
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
 
-//        public override bool Equals(object obj)
-//        {
-//            MyLinkedListNode external = obj as MyLinkedListNode;
-//            if (this.name.Equals(external.name))
-//            { return true; }
+        public MyEnumerator(MyLinkedList mylink)
+        {
+            this._first = mylink._first;
+            _last = mylink._goal;
+        }
 
-//            return false;
-//        }        
+        public bool MoveNext()
+        {
+            if (_index < 0) //_listMax.Count<1
+            {
+                _listMax.Add(new List<MyLinkedListNode>() { _first });
+                SetAll(_first, new List<MyLinkedListNode>() { _first }, _listMax.Count - 1);
+                CheckRightList();
+            }
+            _index++;
 
-//        public int NextCount
-//        {
-//            get
-//            {
-//                return _next.Count;
-//            }
-//        }
+            if (_index >= _finalList.Count)
+                return false;
+            return true;
+        }
 
-//        public int PreviousCount
-//        {
-//            get
-//            {
-//                return _prev.Count;
-//            }
-//        }
-//    }
-//}
+        private void CheckRightList()
+        {
+            foreach (var node in _listMax)
+            {
+                if (node[node.Count - 1].Equals(_last))
+                { _finalList.Add(node); }
+            }
+        }
 
-////AddAfter(LinkedListNode<T>, T)
+        private void SetAll(MyLinkedListNode _first, List<MyLinkedListNode> locked, int index)
+        {
+            var copyListMax = CopyFrom(_listMax[index]);
+            int copyIndex = index;
 
-////Aggiunge un nuovo nodo che contiene il valore specificato dopo il nodo esistente indicato nell'oggetto LinkedList<T>.
+            foreach (var node in _first._next)
+            {
+                List<MyLinkedListNode> copyLocked = CopyFrom(locked);
 
-////System_CAPS_pubmethod AddAfter(LinkedListNode<T>, LinkedListNode<T>)
+                if (locked.Contains(node, node) == false)
+                {
+                    if (copyIndex != index)
+                    {
+                        _listMax.Add(new List<MyLinkedListNode>(copyListMax));
+                        copyIndex = _listMax.Count - 1;
+                    }
+
+                    _listMax[copyIndex].Add(node);
+                    copyLocked.Add(node);
+                    SetAll(node, copyLocked, copyIndex);
+                    index++;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        private T CopyFrom<T>(T list) where T : class, IEnumerable, ICollection, IList, new()
+        {
+            T result = new T();
+            foreach (var n in list)
+            {
+                result.Add(n);
+            }
+            return result;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~MyEnumerator() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+
+    public class MyLinkedListNode : IEqualityComparer<MyLinkedListNode>
+    {
+        internal List<MyLinkedListNode> _next = new List<MyLinkedListNode>();
+        internal List<MyLinkedListNode> _prev = new List<MyLinkedListNode>();
+        internal readonly string name;
+        public Dictionary<string, int> value;
+
+        public MyLinkedListNode(string name, Dictionary<string, int> value = null)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public MyLinkedListNode Next(int n)
+        {
+            return _next[n];
+        }
+        public MyLinkedListNode Previous(int n)
+        {
+            return _prev[n];
+        }
+
+        public override bool Equals(object obj)
+        {
+            MyLinkedListNode external = obj as MyLinkedListNode;
+            if (this.name.Equals(external.name))
+            { return true; }
+
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"({name})";
+        }
+
+        #region interface
+        public bool Equals(MyLinkedListNode x, MyLinkedListNode y)
+        {
+            if (x.name.Equals(y.name))
+            { return true; }
+
+            return false;
+        }
+        public int GetHashCode(MyLinkedListNode obj)
+        {
+            return obj.name.GetHashCode();
+        }
+        #endregion
+
+        public int NextCount
+        {
+            get
+            {
+                return _next.Count;
+            }
+        }
+        public int PreviousCount
+        {
+            get
+            {
+                return _prev.Count;
+            }
+        }
+    }
+}
+
