7a1e64b734637212a4ef0e1d91435bc50f886981
vagnani <vagnani01@gmail.com>
2016-02-29 19:41:06 +0100

    ended class

--- MyLibrary/Collections/Chess.cs
+++ MyLibrary/Collections/Chess.cs
@@ -14,14 +14,14 @@ namespace MyLibrary.Collections
 
         internal Coordinate _limit;
         internal List<Coordinate> _locked;
-        internal Dictionary<string, Coordinate> _award;
+        //da implementare in main Dictionary<string, Coordinate> _award; <==stava anche nel costruttore
         internal List<string> _directions;
 
-        public MyChessBoard() { }
-        public MyChessBoard( Coordinate start, Coordinate arrive, Coordinate limit,
-         List<Coordinate> locked, Dictionary<string, Coordinate> award,List<string> directions)
+        private MyChessBoard() { }
+        public MyChessBoard(Coordinate start, Coordinate arrive, Coordinate limit,
+         List<Coordinate> locked, List<string> directions) : this()
         {
-            _start = start; _arrive = arrive;_limit = limit;_locked = locked; _award = award;_directions = directions;
+            _start = start; _arrive = arrive; _limit = limit; _locked = locked; _directions = directions;
         }
 
         public IEnumerator<List<Coordinate>> GetEnumerator()
@@ -37,23 +37,34 @@ namespace MyLibrary.Collections
     public class MyEnumerator : IEnumerator<List<Coordinate>>
     {
         private int index = -1;
-        private List<List<Coordinate>> _listMax;
-        private List<List<Coordinate>> final;
+        private List<List<Coordinate>> _listMax = new List<List<Coordinate>>();
+        private List<List<Coordinate>> final = new List<List<Coordinate>>();
         private MyChessBoard chess;
 
-        public MyEnumerator() { }
-        public MyEnumerator(MyChessBoard chess):this()
+        public MyEnumerator(MyChessBoard chess)
         {
             this.chess = chess;
             _listMax.Add(new List<Coordinate>() { chess._start });
-            SetAll(chess._start, new List<Coordinate>() {chess._start}, 0);
+            SetAll(chess._start, new List<Coordinate>() { chess._start }, 0);
+            CheckTheRight();
+        }
+
+        private void CheckTheRight()
+        {
+            foreach (var element in _listMax)
+            {
+                if (element[element.Count - 1].Equals(chess._arrive))
+                    final.Add(element);
+            }
         }
 
         public List<Coordinate> Current
         {
             get
             {
-                return final[index];
+                if (index < final.Count)
+                { return final[index]; }
+                throw new ArgumentOutOfRangeException();
             }
         }
         object IEnumerator.Current
@@ -66,13 +77,12 @@ namespace MyLibrary.Collections
 
         public bool MoveNext()
         {
-            if(index>=final.Count)
+            if (index < final.Count - 1)
             {
-                return false;
+                index++;
+                return true;
             }
-
-            index++;
-            return true;
+            return false;
         }
 
         private void SetAll(Coordinate _first, List<Coordinate> locked, int index)
@@ -81,13 +91,14 @@ namespace MyLibrary.Collections
             int copyIndex = index;
 
             foreach (var nodePossible in chess._directions)
-            {                
+            {
                 List<Coordinate> copyLocked = CopyFrom(locked);
                 var possibleCoordinate = MoveByDirection(nodePossible, _first);
 
-                if (possibleCoordinate<chess._limit && 
-                    !chess._locked.Contains(possibleCoordinate, possibleCoordinate) && 
-                    !locked.Contains(possibleCoordinate, possibleCoordinate))
+                if (possibleCoordinate.x <= chess._limit.x && possibleCoordinate.y <= chess._limit.y &&
+                    !chess._locked.Contains(possibleCoordinate, possibleCoordinate) &&
+                    !locked.Contains(possibleCoordinate, possibleCoordinate) &&
+                    possibleCoordinate.x > 0 && possibleCoordinate.y > 0)
                 {
                     if (copyIndex != index)
                     {
@@ -102,11 +113,10 @@ namespace MyLibrary.Collections
                 }
             }
         }
-        
-        private Coordinate MoveByDirection(string direction,Coordinate coor)
+        private Coordinate MoveByDirection(string direction, Coordinate coor)
         {
             #region est 
-            if (direction=="nne")
+            if (direction == "nne")
             {
                 return coor + new int[] { 1, +2 };
             }
@@ -151,7 +161,6 @@ namespace MyLibrary.Collections
 
             return new Coordinate(0, 0);
         }
-
         private T CopyFrom<T>(T list) where T : class, IEnumerable, ICollection, IList, new()
         {
             T result = new T();
@@ -204,39 +213,25 @@ namespace MyLibrary.Collections
 
     }
 
-    public struct Coordinate:IEqualityComparer<Coordinate>
+    public struct Coordinate : IEqualityComparer<Coordinate>
     {
-        int x;
-        int y;
+        internal int x;
+        internal int y;
 
-        public Coordinate(int x,int y)
+        public Coordinate(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
 
-        public static bool operator >(Coordinate obj1,Coordinate obj2)
-        {
-            if(obj1.x>obj2.x && obj1.y>obj2.y)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static bool operator <(Coordinate obj1, Coordinate obj2)
-        {
-            if (obj1.x < obj2.x && obj1.y < obj2.y)
-            {
-                return true;
-            }
-            return false;
-        }
-
         public override bool Equals(object obj)
         {
             var obj1 = (Coordinate)obj;
-            return this.x==obj1.x && this.y==obj1.y;
+            return this.x == obj1.x && this.y == obj1.y;
+        }
+        public override string ToString()
+        {
+            return $"[{x},{y}]";
         }
 
         #region IEqualityCmparer
@@ -256,21 +251,199 @@ namespace MyLibrary.Collections
         {
             return obj1.Equals(obj2);
         }
-
         public static bool operator !=(Coordinate obj1, Coordinate obj2)
         {
-            if(obj1.Equals(obj2))
+            if (obj1.Equals(obj2))
             {
                 return false;
             }
 
             return true;
         }
-        
-        public static Coordinate operator +(Coordinate obj1,int[] xy)
+
+        public static Coordinate operator +(Coordinate obj1, int[] xy)
         {
             return new Coordinate(obj1.x + xy[0], obj1.y + xy[1]);
         }
+
+        public static bool operator >(Coordinate obj1, Coordinate obj2)
+        {
+            if (obj1.x > obj2.x && obj1.y > obj2.y)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <(Coordinate obj1, Coordinate obj2)
+        {
+            if (obj1.x < obj2.x && obj1.y < obj2.y)
+            {
+                return true;
+            }
+            return false;
+        }
         #endregion
+    }
+
+    public static class Adding
+    {
+        public static Coordinate CreateCoordinateByString(string str)
+        {
+            char excluded1 = '(';
+            char excluded2 = ')';
+            char excluded3 = ',';
+            List<string> temp = new List<string>();
+
+            str.Trim();
+
+            //if (str[index] == excluded1)                
+            string name = "";
+            for (int index = 0; index <= str.Count(); index++)
+            {
+                if (str[index] != excluded1 && str[index] != excluded2 && str[index] != excluded3) //exception
+                {
+                    name += str[index];
+                }
+
+                if (str[index] == excluded3)
+                {
+                    temp.Add(name);
+                    name = "";
+                }
+
+                if (str[index] == excluded2)
+                {
+                    temp.Add(name);
+                    break;
+                }
+            }
+
+            return new Coordinate(Convert.ToInt32(temp[0]), Convert.ToInt32(temp[1]));
+        }
+
+        public static List<Coordinate> CreateLockedCells(string str)
+        {
+            char excluded1 = '(';
+            char excluded2 = ')';
+            char excluded3 = ',';
+            List<List<string>> lockedString = new List<List<string>>();
+            List<Coordinate> locked = new List<Coordinate>();
+
+            str.Trim();
+
+            for (int index = 0; index < str.Count(); index++)
+            {
+                List<string> temp = new List<string>();
+                //if (str[index] == excluded1)                
+                string name = "";
+                for (; index < str.Count(); index++)
+                {
+                    if (str[index] != excluded1 && str[index] != excluded2 && str[index] != excluded3) //exception
+                    {
+                        name += str[index];
+                    }
+
+                    if (str[index] == excluded3)
+                    {
+                        temp.Add(name);
+                        name = "";
+                    }
+
+                    if (str[index] == excluded2)
+                    {
+                        temp.Add(name);
+                        break;
+                    }
+                    lockedString.Add(temp);
+                }
+            }
+
+            foreach (var element in lockedString)
+            {
+                var x = Convert.ToInt32(element[0]);
+                var y = Convert.ToInt32(element[1]);
+                locked.Add(new Coordinate(x, y));
+            }
+
+            return locked;
+        }
+
+        public static List<string> CreateDirections(string str)
+        {
+            char excluded1 = '(';
+            char excluded2 = ')';
+            List<string> directions = new List<string>();
+
+            str.Trim();
+
+            for (int index = 0; index < str.Count(); index++)
+            {
+                //if (str[index] == excluded1)                
+                string name = "";
+                for (; index < str.Count(); index++)
+                {
+                    if (str[index] != excluded1 && str[index] != excluded2) //exception
+                    {
+                        name += str[index];
+                    }
+
+                    if (str[index] == excluded2)
+                    {
+                        directions.Add(name);
+                        break;
+                    }
+                }
+            }
+            return directions;
+        }
+
+        public static Dictionary<string, Coordinate> CreateAwards(string str)
+        {
+            char excluded1 = '(';
+            char excluded2 = ')';
+            char excluded3 = ',';
+            List<List<string>> all = new List<List<string>>();
+            Dictionary<string, Coordinate> finalAll = new Dictionary<string, Coordinate>();
+
+            str.Trim();
+
+
+            for (int index = 0; index < str.Count(); index++)
+            {
+                List<string> temp = new List<string>();
+
+                //if (str[index] == excluded1)                
+                string name = "";
+                for (; index <= str.Count(); index++)
+                {
+                    if (str[index] != excluded1 && str[index] != excluded2 && str[index] != excluded3) //exception
+                    {
+                        name += str[index];
+                    }
+
+                    if (str[index] == excluded3)
+                    {
+                        temp.Add(name);
+                        name = "";
+                    }
+
+                    if (str[index] == excluded2)
+                    {
+                        temp.Add(name);
+                        break;
+                    }
+                }
+
+                all.Add(temp);
+            }
+
+            foreach (var item in all)
+            {
+                int x = Convert.ToInt32(item[1]);
+                int y = Convert.ToInt32(item[2]);
+                finalAll.Add(item[0], new Coordinate(x, y));
+            }
+            return finalAll;
+        }
     }
 }
